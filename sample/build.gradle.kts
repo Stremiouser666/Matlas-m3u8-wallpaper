60ac60c749b83160b47d1c0de650aef94013eb06
Steveies <protonhollis@gmail.com>
2025-12-18 02:25:53 +0000

    Initial commit

---
+++ sample/build.gradle.kts
@@ -0,0 +1,53 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "tv.projectivy.plugin.wallpaperprovider.sample"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "tv.projectivy.plugin.wallpaperprovider.sample"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.01"
+
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlin {
+        compilerOptions {
+            jvmTarget.set(JvmTarget.JVM_17)
+        }
+    }
+
+    buildFeatures {
+        buildConfig = true
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.leanback:leanback:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.7.1")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.preference:preference-ktx:1.2.1")
+    implementation("com.google.code.gson:gson:2.13.1")
+    implementation(project(":api"))
+}
